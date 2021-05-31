90ea63402a8d33f115aebe07e6d923a8061d7670
Riley <riley.childs@yahoo.com>
2021-05-31 19:12:36 -0400

    5-31-2021

--- index.php
+++ index.php
@@ -2,13 +2,22 @@
 <?php include 'include/header.php';?>
 
 <div id="content">
-<h2>Just Your Friendly Neighborhood Fullstack Developer</h2>
 
+<div id="aboutMe">
+<h4>Your friendly Neighborhood Fullstack Developer</h4>
+<p>I am an AASI Level 3 certified snowboard instructor 
+    looking to join Woodward Copper’s team of summer
+     snowboard coaches. I have taught guests of all 
+     ages and ability levels over the eight seasons 
+     that I have been coaching. Coaching park and 
+     freestyle riding is one of my areas of expertise
+      as a coach. Whenever there is a need for a park
+       lesson, I am always the one who takes charge. </p>   
+<img src="images/euroCarve.jpg">
+</div>
 
-
-
-<h2>Areas of Expertise</h2>
-<div id = "languages">   
+<div id = "languages">  
+<h4>Areas of Expertise</h4> 
 <img class="frontEnd" src="images/languages/html+css+JS.png" alt="HTML 5, CSS3, and JavaScript(ES6)">
 <img class="backEnd" src="images/languages/php+MySQL.png" alt="PHP and MySQL">
 </div>
