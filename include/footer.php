de2345869cdc5eb4e48c957893affc95f99ea359
Riley <riley.childs@yahoo.com>
2021-06-04 20:56:59 -0400

    Reformatted footer

--- include/footer.php
+++ include/footer.php
@@ -1,11 +1,6 @@
 <footer>
 <?php include 'contact.php'; ?>
-<div id="C2A">
-<h4>Get in Touch</h4>
-<p>Shoot me an email through the form on the right. We'll work
-    out a plan for your web-based needs
-</p>
-</div>
+
 <div id="socialMedia">
     <h4>Social Media</h4>
     <a href="">LinkedIn</a>
