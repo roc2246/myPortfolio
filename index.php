3c01a3809a522254cefd9d9add681e2797a0e852
Riley <riley.childs@yahoo.com>
2021-06-07 08:14:21 -0400

    Bootstrap resizing

--- index.php
+++ index.php
@@ -4,24 +4,24 @@
 <div class="container">
 
 <div class="row">
-<div class="page-header">
-<h4>Your friendly Neighborhood Fullstack Developer</h4>
-</div>
-</div>
-<div class="row">
-<div class="col-sm-3">
-<img id="ec" src="images/euroCarve.jpg">
-</div>
-   <div class="col-sm-9"> 
-<p class="font-weight-lighter" id="main">I am a fullstack web developer located in beautiful Vermont. For over twenty years, I have spent most of my time 
-    outside living the snowboarder life. Not only was I adventurous, but wasa also analytical as well. I always wanted to 
-dissect and break down everything I encoutner in my path.  
-<br><br>Ultimately, I turned to web development to satisfy this need. It all started when I began customizing my MySpace page with custom
-    made HTML and CSS. Eventually, that lkead to me taking more calsses in web development, further solidifying my developer skills. 
-    Since then, web development and coding became some of the biggest passions in my life.
-</p> 
+    <div class="page-header">
+        <h4>Your friendly Neighborhood Fullstack Developer</h4>
+    </div>
 </div>
 
+<div class="row">
+    <div class="col-xs-4">
+        <img id="ec" src="images/euroCarve.jpg">
+    </div>
+   <div class="col-xs-8"> 
+        <p class="font-weight-lighter" id="main">I am a fullstack web developer located in beautiful Vermont. For over twenty years, I have spent most of my time 
+         outside living the snowboarder life. Not only was I adventurous, but wasa also analytical as well. I always wanted to 
+        dissect and break down everything I encoutner in my path.  
+        <br><br>Ultimately, I turned to web development to satisfy this need. It all started when I began customizing my MySpace page with custom
+          made HTML and CSS. Eventually, that lkead to me taking more calsses in web development, further solidifying my developer skills. 
+         Since then, web development and coding became some of the biggest passions in my life.
+        </p> 
+    </div>
 </div>
 
 
@@ -39,10 +39,10 @@ dissect and break down everything I encoutner in my path.
 </div>
 </div>
 <div class="row">
-<div class="col-sm-3">
+<div class="col-xs-3">
 <img style="border: solid;" src="images/projects/after3.png" alt="">
 </div>
-<div class="col-sm-9">
+<div class="col-xs-9">
 <p id="main">A mockup site of a travel destination page. Included 
          are pages for each resort, a randomnly-generated movie of the week, an 
          AJAX powered quiz, and much more.<br><br>
@@ -52,5 +52,14 @@ dissect and break down everything I encoutner in my path.
 </div>
 
 </div>
+
+<script>
+if(window.outerWidth > 768){
+    alert("test");
+    document.getElementsByTagName('div')[4].setAttribute("class", "col-sm-4");
+    document.getElementsByTagName('div')[5].setAttribute("class", "col-sm-8");
+}
+</script>
+
 
 <?php include "include/footer.php"?>
