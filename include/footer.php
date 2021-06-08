ebdfffd0fa36f584f1dbfc452c0523923444d3e4
Riley <riley.childs@yahoo.com>
2021-06-07 21:02:07 -0400

    Fixed 'Social Media'

--- include/footer.php
+++ include/footer.php
@@ -13,15 +13,22 @@
 </div>
 
 <div class="col-sm-1.5">
+    <div class="row justify-content-center">  
+     <div class="page-header">
     <h4>Social Media</h4>
-    <a href="">LinkedIn</a>
-    <a href="">GitHub</a>
+    </div>
+    </div>
+ 
+    <div class="row justify-content-center">
+    <a href="http://www.linkedin.com/in/riley-childs-4b015b1a4"><img src="images/socialMedia/linkedIn.png" alt="LinkedIn"></a>
+    <a href=""><img src="images/socialMedia/gitHub.png" alt="gitHub"></a>
+  </div>
 </div>
 
 </div>
-<div class="row">
+<div class="row mt-4">
 <div class="col-sm-12">
-<p id="copyright">Copyright 2020</p>
+<p id="copyright">Copyright 2021</p>
 </div>
 </div>
 </div>
