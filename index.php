6cf31510abf64000be8800d6dd3e703cea4ae625
Riley <riley.childs@yahoo.com>
2021-06-08 09:49:37 -0400

    Spellcheck

--- index.php
+++ index.php
@@ -15,10 +15,10 @@
     </div>
    <div class="col-sm-9"> 
         <p class="font-weight-lighter" id="main">I am a fullstack web developer located in beautiful Vermont. For over twenty years, I have spent most of my time 
-         outside living the snowboarder life. Not only was I adventurous, but wasa also analytical as well. I always wanted to 
+         outside living the snowboarder life. Not only was I adventurous, but was also analytical as well. I always wanted to 
         dissect and break down everything I encoutner in my path.  
         <br><br>Ultimately, I turned to web development to satisfy this need. It all started when I began customizing my MySpace page with custom
-          made HTML and CSS. Eventually, that lkead to me taking more calsses in web development, further solidifying my developer skills. 
+          made HTML and CSS. Eventually, that lead to me taking more classes in web development, further solidifying my developer skills. 
          Since then, web development and coding became some of the biggest passions in my life.
         </p> 
     </div>
