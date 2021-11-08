f9fa54964f8d430a308d8c85b628b8028d6406d5
Riley <riley.childs@yahoo.com>
2021-11-08 07:43:20 -0500

    Deleted unnecessary files

--- blank.php
+++
@@ -1,8 +0,0 @@
-<?php $pageTitle = "";?>
-<?php include 'include/header.php';?>
-<?php include 'include/navigation.php';?>
-<div id="content">
-
-</div>
-
-<?php include "include/footer.php"?>
--- experience.php
+++
@@ -1,8 +0,0 @@
-<?php $pageTitle = "Work Experience";?>
-<?php include 'include/header.php';?>
-<?php include 'include/navigation.php';?>
-<div id="content">
-
-</div>
-
-<?php include "include/footer.php"?>
