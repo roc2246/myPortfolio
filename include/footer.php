db31fb5d24e46d75b14a3f94fcb6c962105fd0c4
Riley <riley.childs@yahoo.com>
2021-11-19 18:57:17 -0500

    Linted PHP components

--- include/footer.php
+++ include/footer.php
@@ -1,3 +1,17 @@
+<?php 
+/** 
+ * Footer for Riley Child's Web Portfolio
+ * 
+ * PHP version 7.4
+ * 
+ * @category Element_Groups
+ * @package  Components
+ * @author   Riley Childs <riley.childs@yahoo.com>
+ * @license  Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)
+ * @link     http://wh963069.ispot.cc/include/footer.php
+ */
+?>
+
 <footer>
 
 <div class="container-fluid">
--- include/header.php
+++ include/header.php
@@ -1,10 +1,26 @@
+<?php 
+/** 
+ * Header for Riley Child's Web Portfolio
+ * 
+ * PHP version 7.4
+ * 
+ * @category Element_Groups
+ * @package  Components
+ * @author   Riley Childs <riley.childs@yahoo.com>
+ * @license  Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)
+ * @link     http://wh963069.ispot.cc/include/header.php
+ */
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" 
+    href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="./style.css">
     <link rel="shortcut icon" href="#">
     <title>Riley Childs - <?php echo $pageTitle; ?></title>
