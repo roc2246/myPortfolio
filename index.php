dca914ca7c7b4431b58a5c4aa3c5b60513a94f1f
Riley <riley.childs@yahoo.com>
2022-03-25 11:11:56 -0400

    Fixed flexbox issue with Projects

--- index.php
+++ index.php
@@ -59,40 +59,42 @@ require 'include/header.php';
 
   <!--Projects-->
   <div class="selected-option">
-    <div class="project">
-      <p class="paragraph-main">
-      <img class="project-image" src="images/projects/after3.png" alt="After 3">  
-      A mockup site of a travel destination page. Included 
-        are pages for each resort, a randomnly-generated movie of the week, an 
-        AJAX powered quiz, and much more.<br><br>
-        <a href= "http://wh963069.ispot.cc/projects/after3/index.php">
-          Check this project out!
-        </a> <br>
-        <a href="https://github.com/roc2246/after3">Source Code</a> 
-      </p>
-    </div>
-    <div class="project">
-      <p class="paragraph-main">
-        <img class="project-image" src="images/projects/theCondo.png" 
-        alt="The Condo">
-        An eCommerce site that sells snowboarding softgoods and hardgoods.<br><br>
-        <a href="http://wh963069.ispot.cc/projects/eCommerce/index.php">
-          Check this project out!
-        </a> <br>
-        <a href="https://github.com/roc2246/eCommerceMockup">Source Code</a> 
-      </p>
-    </div>
-    <div class="project">
-      <p class="paragraph-main">
-        <img class="project-image" src="images/projects/reCRUD.png" 
-        alt="Real Estate App">
-        A web application that consumes a self-coded real esate API. CRUD 
-        features are used to manage the listings.<br><br>
-        <a href="http://wh963069.ispot.cc/projects/real_estate/index.php">
-          Check this project out!
-        </a> <br>
-        <a href="https://github.com/roc2246/real_estate">Source Code</a> 
-      </p>
+    <div id="project-flex-container">
+      <div class="project">
+        <p class="paragraph-main">
+        <img class="project-image" src="images/projects/after3.png" alt="After 3">  
+        A mockup site of a travel destination page. Included 
+          are pages for each resort, a randomnly-generated movie of the week, an 
+          AJAX powered quiz, and much more.<br><br>
+          <a href= "http://wh963069.ispot.cc/projects/after3/index.php">
+            Check this project out!
+          </a> <br>
+          <a href="https://github.com/roc2246/after3">Source Code</a> 
+        </p>
+      </div>
+      <div class="project">
+        <p class="paragraph-main">
+          <img class="project-image" src="images/projects/theCondo.png" 
+          alt="The Condo">
+          An eCommerce site that sells snowboarding softgoods and hardgoods.<br><br>
+          <a href="http://wh963069.ispot.cc/projects/eCommerce/index.php">
+            Check this project out!
+          </a> <br>
+          <a href="https://github.com/roc2246/eCommerceMockup">Source Code</a> 
+        </p>
+      </div>
+      <div class="project">
+        <p class="paragraph-main">
+          <img class="project-image" src="images/projects/reCRUD.png" 
+          alt="Real Estate App">
+          A web application that consumes a self-coded real esate API. CRUD 
+          features are used to manage the listings.<br><br>
+          <a href="http://wh963069.ispot.cc/projects/real_estate/index.php">
+            Check this project out!
+          </a> <br>
+          <a href="https://github.com/roc2246/real_estate">Source Code</a> 
+        </p>
+      </div>
     </div>
   </div>
 
